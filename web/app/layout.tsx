b418e0a9e84769d0838f743628b7e3be5dabd524
Steve Hayton <73317139+stevehayton@users.noreply.github.com>
2026-04-11 02:42:35 -0600

    GA

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -26,6 +26,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-8R4HG16TTS"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-8R4HG16TTS');
+`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
